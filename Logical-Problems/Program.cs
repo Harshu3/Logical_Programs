bef58d0e0a602485bd41ba736d22ff5823589cd5
Harshita <chandaliyaharshita222@gmail.com>
2023-04-19 21:37:18 +0530

    Adding Fibonacci Problem

---
+++ Logical-Problems/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Logical_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Problem!");
+            Console.WriteLine("Fibonacci Program");
+            Fibonacci fibonacci = new Fibonacci();
+            fibonacci.Fibo();
+        }
+    }
+}
